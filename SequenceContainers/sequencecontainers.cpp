507b7fd2a709bb27b37d135c8ce974c9fe0dd486
user <user@WIN-CP209TC39O5>
2025-10-05 21:45:44 +0300

    Sequence containers DONE

--- SequenceContainers/sequencecontainers.cpp
+++ SequenceContainers/sequencecontainers.cpp
@@ -1,16 +1,24 @@
 #include <iostream>
 #include <array>
 #include <vector>
+#include <deque>
+#include <list>
+#include <forward_list>
+#include <iterator>
 using std::cin;
 using std::cout;
 using std::endl;
+#define tab "\t"
+#define delimeter "\n------------------------------\n"
 
 template<typename T> void vector_info(const std::vector<T>& vec);
+template<typename T>void deque_info(const std::deque<T>& vec);
 
 //#define STL_ARRAY
 #define STIL_VECTOR
-#define tab "\t"
-#define delimeter "\n------------------------------\n"
+#define STL_DEQUE
+//#define STL_LIST
+//#define STL_FORWARD_LIST
 
 void main()
 {
@@ -46,6 +54,11 @@ void main()
 #endif
 
 #ifdef STIL_VECTOR
+	/*
+	Vector
+
+	Это контейнер, который хранит данные, в виде динамического массива
+	*/
 	std::vector<int> vec = { 0,1,1,2,3,5,8,13,21,34 };
 	for (int i = 0; i < vec.size(); i++)
 	{
@@ -79,7 +92,104 @@ void main()
 	} while (index > vec.capacity());
 	vec.insert(vec.begin() + index, value);
 	for (int i : vec)cout << i << tab; cout << endl;
+
+	do
+	{
+		cout << "Введите индекс добавляемого элемента: "; cin >> index;
+	} while (index > vec.capacity());
+	vec.erase(vec.begin() + index);
+	for (int i : vec)cout << i << tab; cout << endl;
 #endif // STIL_VECTOR
+
+#ifdef STL_DEQUE
+	//deque - это контейнер, который хранит данные в виде списка векторов.
+	//Это позволяет обеспечить относительно быстрый доступ к элементам, и относительно быстрое добавление и удаление элементов.
+	//deque - double-ended queue (двунаправленная очередь).
+	//deque по своим возможностям очень напоминает vector, и дополняет его методами push_front и pop_front.
+
+	std::deque<int> deque = { 3, 5, 8, 13, 21 };
+	deque.push_back(34);
+	deque.push_front(2);
+
+	/*std::vector<int>vec;
+	cout << typeid(vec.data()).name() << endl;*/
+
+	deque_info(deque);
+	deque.assign(vec.begin() + 3, vec.end() - 3);
+	for (int i : deque)cout << i << tab; cout << endl;
+
+	/*
+	В то время как vector гарантированно хранит данные в одном массиве, для изменения размера которого, его нужно переопределить - элементы deque могут быть разбросаны в памяти.
+	Именно поэтому vector предоставляет доступ к хранилищу в виде указателя на массив, а deque этого не делает.
+
+	Deque внутренне устроен сложнее, чем вектор. Это позволяет ему изменять размер гораздо эффективнее при тех же обстоятельствах, что и вектор, особенно при больших размерах.
+	*/
+
+#endif // STL_DEQUE
+
+#ifdef STL_LIST
+	//List - это контейнер, который хранит данные в виде двусвязного списка.
+	std::list<int> list = { 3, 5, 8, 13, 21, 34, 55, 89 };
+	list.push_front(2);
+	list.push_front(1);
+	list.push_front(1);
+	list.push_front(0);
+	for (std::list<int>::iterator it = list.begin(); it != list.end(); ++it)
+		cout << *it << tab;
+	cout << endl;
+
+	for (std::list<int>::reverse_iterator it = list.rbegin(); it != list.rend(); ++it)
+		cout << *it << tab;
+	cout << endl;
+
+	int index;
+	int value;
+	cout << "Введите индекс добавляемого элемента: "; cin >> index;
+	cout << "Введите значение добавляемого элемента: "; cin >> value;
+	//list.insert(list.begin() + index, value);     //Списки не поддерживают арифметику указателей
+	std::list<int>::iterator position = list.begin();
+	//for (int i = 0; i < index; i++)position++;
+	//std::advance(position, index);    //Функция advande() смещает итератор вправо, на заданное количество элементов.
+	//list.insert(position, value);
+
+	std::list<int>::reverse_iterator r_positiion = list.rbegin();
+	std::advance(r_positiion, list.size() - index);
+	position = r_positiion.base();
+	list.insert(position, value);
+
+	for (int i : list)cout << i << tab; cout << endl;
+
+	cout << "Введите индекс добавляемого элемента: "; cin >> index;
+	position = list.begin();
+	std::advance(position, index);
+	list.erase(position);
+	for (int i : list)cout << i << tab; cout << endl;
+
+#endif // STL_LIST
+
+#ifdef STL_FORWARD_LIST
+
+	std::forward_list<int> list = { 3, 5, 8, 13, 21 };
+	list.push_front(2);
+	list.push_front(1);
+	list.push_front(1);
+	list.push_front(0);
+	for (std::forward_list<int>::iterator it = list.begin(); it != list.end(); ++it)
+		cout << *it << tab;
+	cout << endl;
+
+	int index;
+	int value;
+	cout << "Введите индекс добавляемого элемента: "; cin >> index;
+	cout << "Введите значение добавляемого элемента: "; cin >> value;
+	std::forward_list<int>::iterator position = list.before_begin();
+	std::advance(position, index);
+	list.insert_after(position, value);
+	list.insert_after(list.begin(), 16384);
+	for (int i : list)cout << i << tab; cout << endl;
+
+#endif // STL_FORWARD_LIST
+
 
 }
 
@@ -88,6 +198,13 @@ template<typename T> void vector_info(const std::vector<T>& vec)
 	cout << "Size: " << vec.size() << endl;
 	cout << "MaxSize: " << vec.max_size() << endl;
 	cout << "Capacity: " << vec.capacity() << endl; //Объем зарезервированной памяти. Зарезервированная память нужна для того, чтобы ускорить процесс добавления элементов и избежать необходимости переопределять память при каждом добавлении элемента.
+	cout << delimeter << endl;
+}
+template<typename T>void deque_info(const std::deque<T>& vec)
+{
+	cout << "Size:\t " << vec.size() << endl;		//Фактический размер Вектора, показывающий сколько элементов хранит Вектор.
+	cout << "MaxSize: " << vec.max_size() << endl;
+	//cout << "Capacity:" << vec.capacity() << endl;	
 	cout << delimeter << endl;
 }
 	/*
@@ -120,9 +237,4 @@ template<typename T> void vector_info(const std::vector<T>& vec)
 	У одного блока try не может быть двух однотипных обработчиков, но может быть один универсальный.
 
 	Обработка исключения позволяет восстановить работу без аварийного завершения.
-	*/
-	/*
-	Vector
-
-	Это контейнер, который хранит данные, в виде динамического массива
 	*/
